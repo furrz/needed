03fbce417e192b109782ad39274b8997961abd48
nova <soaproap@gmail.com>
2016-09-25 17:28:33 -0400

    WHat

--- main.cpp
+++ main.cpp
@@ -18,14 +18,14 @@ int main()
         return -1;
     }
 
-    glfwWindowHint(GLFW_SAMPLES, 0); // Fuck antialiasing.. speed is good!
+    glfwWindowHint(GLFW_SAMPLES, 4); // Fuck antialiasing.. speed is good!
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.3!!
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS wants this shit.
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Fuck older OpenGL.
 
     GLFWwindow* window;
-    window = glfwCreateWindow( 1000, 600, "Thingy", NULL, NULL);
+    window = glfwCreateWindow( WIDTH, HEIGHT, "Thingy", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n" );
         glfwTerminate();
@@ -39,6 +39,10 @@ int main()
     }
 
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
 
     static const GLfloat g_vertex_buffer_data[] = {
        -1.0f, -1.0f, 0.0f,
@@ -55,6 +59,9 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
     do{
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         glVertexAttribPointer(
@@ -67,8 +74,8 @@ int main()
         );
         // Draw the triangle !
         glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-        glDisableVertexAttribArray(0);
 
+        glDisableVertexAttribArray(0);
 
         // Swap buffers
         glfwSwapBuffers(window);
